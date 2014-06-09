dad02619689c0e172d48273cf77dab94652e2df4
Georgi K <georgiknox@gmail.com>
2014-06-09 17:33:17 -0400

    ModulesAndFunctions ans

---
+++ times.exs
@@ -0,0 +1,18 @@
+# ModulesAndFunctions-1
+# ModulesAndFunctions-2
+
+
+defmodule Times do
+
+  def double(n) do
+    n * 2
+  end
+
+  def triple(n) do
+    n * 3
+  end
+
+  def quadruple(n) do
+    double(n) + double (n)
+  end
+end
